18c65c51733c03360f2159ef8a0e676bdbe1b866
LAPTOP-3Q7N09F8\saibh <sai.bhavana0907@gmail.com>
2024-09-09 02:21:52 -0500

    Madlibs project created

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,22 @@
+# color = input("Enter any color: ")
+# phrase = input("Enter your favorite phrase: ")
+#
+# print("Chocolate is " + color)
+# print("But you say " + phrase)
+
+# string concatenation (to put strings together)
+# different ways to do it -
+# youtuber = "me"
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!!"
+
+print(madlib)
